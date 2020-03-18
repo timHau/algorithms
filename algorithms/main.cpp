1bf1e216b09b7fce40b2ad6d5cfda357b59444e9
timHau <tim.hau@hotmail.de>
2020-03-18 10:36:45 +0100

    Initial Commit

---
+++ algorithms/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  algorithms
+//
+//  Created by tim on 18.03.20.
+//  Copyright © 2020 tim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
